b67ede026a91d19c43a517ddd6ee108e5482e5fd
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-08-02 23:59:48 -0300

    feat: scss fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 'use client';
 
-//.jpg
-import Summer from '@/assets/images/summer.jpg';
-import Spring from '@/assets/images/spring.jpg';
-import Winter from '@/assets/images/winter.jpg';
-import Autumn from '@/assets/images/autumn.jpg';
-import ProfileIcon from '@/assets/images/profile.jpg';
-//.svg
-import Facebook from '@/assets/svgs/facebook-color.svg';
-import Twitter from '@/assets/svgs/twitter-color.svg';
-import Instagram from '@/assets/svgs/instagram-black.svg';
-import Github from '@/assets/svgs/github-color.svg';
-import Youtube from '@/assets/svgs/youtube-color.svg';
 import Button from '@/components/button';
 
 export default function Home() {
   return (
-    <main style={{ gap: 10 }}>
+    <div
+      style={{
+        gap: 10,
+        flex: 1,
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <h3>Button</h3>
       <Button>PRIMARY</Button>
       <Button disabled>PRIMARY</Button>
       <Button variant="outline">OUTLINE</Button>
@@ -24,6 +20,11 @@ export default function Home() {
         OUTLINE
       </Button>
       <Button variant="text">TEXT</Button>
-    </main>
+      <Button size="large">Large Button</Button>
+      <Button size="middle">Middle Button</Button>
+      <Button size="small">Small Button</Button>
+
+      <h3>Input / TextField</h3>
+    </div>
   );
 }
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,5 @@
 import React, { ButtonHTMLAttributes } from 'react';
-import './styles.scss';
+import './_styles.scss';
 
 type ButtonProps = {
   children?: React.ReactNode;
