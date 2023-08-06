b40dbae2a9cfa0e30bc8f074925d66a30ac9275e
Nilton Junior <nilton.junior@digitalbusiness.com.br>
2023-08-06 00:09:13 -0300

    feat: i need to add padding in select scroll

--- src/mock/select-mock.ts
+++ src/mock/select-mock.ts
@@ -2,6 +2,18 @@ export const Options = [
   { value: 'option1', label: 'Opção 1' },
   { value: 'option2', label: 'Opção 2' },
   { value: 'option3', label: 'Opção 3' },
-  { value: 'option4', label: 'Opção 4' }
+  { value: 'option5', label: 'Opção 4' },
+  { value: 'option6', label: 'Opção 4' },
+  { value: 'option7', label: 'Opção 4' },
+  { value: 'option8', label: 'Opção 4' },
+  { value: 'option9', label: 'Opção 4' },
+  { value: 'option10', label: 'Opção 4' },
+  { value: 'option11', label: 'Opção 4' },
+  { value: 'option12', label: 'Opção 4' },
+  { value: 'option13', label: 'Opção 4' },
+  { value: 'option14', label: 'Opção 4' },
+  { value: 'option15', label: 'Opção 4' },
+  { value: 'option16', label: 'Opção 4' },
+  { value: 'option17', label: 'Opção 4' }
   // Adicione mais opções conforme necessário
 ];
